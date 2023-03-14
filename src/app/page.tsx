bd3281299ce3d23ccd482532892d96030daab831
thao <mrtrados@gmail.com>
2023-03-14 22:52:26 +0900

    UPDATE page by import {WeatherCard}

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from './page.module.css'
+import { WeatherCard } from '@/components/WeatherCard'
 
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+    <h1 className='text-6xl'>
+      <WeatherCard/>
+      </h1>
+    </>
   )
 }
