bfad6ecbce9b3bbb9453bb2a2e734e55e4c9e694
thao <mrtrados@gmail.com>
2023-03-15 12:27:23 +0900

    UPDATE WeatherCard

--- src/components/WeatherCard.tsx
+++ src/components/WeatherCard.tsx
@@ -1,29 +1,17 @@
 import { Weather } from "./Database"
 import { format } from "date-fns";
 
-function RenderData(): Weather {
-    return {
-        icon: "",
-        date: new Date(),
-        temperature: 20.5
-    }
-}
 
-export function WeatherCard() {
-    const item: Weather = RenderData();
+export function WeatherCard(prop: any) {
+    const item: Weather = prop.day;
     const date: string = format(item.date, "LLLL dd");
 
     return (
         <>
     <div>
-        {item.icon} 
-        </div>
-    <div>
-    {date}
-        </div>
-    <div>
-    {item.temperature}
-        </div>
+     {date} - {item.temperature}°C
+    </div>
+    <img src={item.icon} alt="imageIcon" width={200} height={200}/>
         </>
     )
 
