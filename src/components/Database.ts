fc9254db170704af082c653c6602e675f471144a
thao <mrtrados@gmail.com>
2023-03-16 20:56:28 +0900

    UPDATE Database with WeatherForecast

--- src/components/Database.ts
+++ src/components/Database.ts
@@ -1,7 +1,18 @@
 export interface Weather {
 
     icon: string;
-    date: Date;
     temperature: number;
+    date: Date;
 
+}
+
+export interface WeatherForecast {
+    id: string;
+    icon: string;
+    temperature: number;
+    condition: string;
+    isPrecipitating: boolean;
+    humidity: number;
+    date: Date;
+    
 }
