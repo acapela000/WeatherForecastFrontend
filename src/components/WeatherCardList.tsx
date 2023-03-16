0e29db95af033f04e697f2b10d576937ff991472
thao <mrtrados@gmail.com>
2023-03-16 20:55:48 +0900

    UPDATE WeatherForecastList following WeatherForecast

--- src/components/WeatherCardList.tsx
+++ src/components/WeatherCardList.tsx
@@ -1,37 +1,64 @@
-import { Weather } from "./Database";
+import { WeatherForecast } from "./Database";
 import { WeatherCard } from "./WeatherCard";
 
-const itemList: Weather[] = 
+
+const itemList: WeatherForecast[] = 
+// id: string;
+//     icon: string;
+//     temperature: number;
+//     condition: string;
+//     isPrecipitating: boolean;
+//     humidity: number;
+//     date: Date;
     [
-        {icon: 'https://icon-library.com/images/weather-icon-gif/weather-icon-gif-4.jpg',
+        {id:'',
+        icon: 'https://icon-library.com/images/weather-icon-gif/weather-icon-gif-4.jpg',
         date: new Date(),
-        temperature: 28.5},
-        {icon: 'https://icon-library.com/images/weather-icon-gif/weather-icon-gif-3.jpg',
+        temperature: 28.5,
+        condition:'sunny',
+        isPrecipitating: false,
+        humidity: 0.7},
+        {id:'',
+        icon: 'https://icon-library.com/images/weather-icon-gif/weather-icon-gif-3.jpg',
         date: new Date(),
-        temperature: 20.5},
-        {icon: 'https://icon-library.com/images/weather-icon-gif/weather-icon-gif-2.jpg',
+        temperature: 20.5,
+        condition:'rainy',
+        isPrecipitating: true,
+        humidity: 0.8},
+        {id:'',
+        icon: 'https://icon-library.com/images/weather-icon-gif/weather-icon-gif-2.jpg',
         date: new Date(),
-        temperature: 24.5}        
+        temperature: 24.5,
+        condition:'windy',
+        isPrecipitating: false,
+        humidity: 0.9}        
     ]
     
 
 export function WeatherCardList() {
-    const itemHTMLList = itemList.map((item, index) => {
+    // const itemHTMLList = itemList.map((item, index) => {
     
-    return (
-        <>
-        <li key={index}>
-            <WeatherCard day={item}/>
-        </li>
-        </>
-    )
-    }) 
+    // return (
+    //     <>
+    //     <li key={item.icon}>
+    //         <WeatherCard day={item}/>
+    //     </li>
+    //     </>
+    // )
+    // }) 
+
+    const itemHTMLList = [];
+    for(var item of itemList) {
+        itemHTMLList.push(
+        <WeatherCard key={item.icon} day={item}/>
+        )
+    }
 
     return(
         <>
-        <ul>
-            {itemHTMLList}
-        </ul>
+        <div className="m-10 items-center flex flex-col md:flex-row md:justify-center">
+        {itemHTMLList}
+        </div>
         </>
     )
 
