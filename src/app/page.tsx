1ff40a01e95ca2bc55fc687de9f447e74095fabb
thao <mrtrados@gmail.com>
2023-03-16 20:49:29 +0900

    ADD SearchBar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
-import { WeatherCardList } from './../components/WeatherCardList';
+import { WeatherCardList } from '@/components/WeatherCardList';
+import { SearchBar } from '@/components/SearchBar';
 
 
 export default function Home() {
   return (
     <>
+    <SearchBar/>
+    
     <h1 className='text-2xl'>
       <WeatherCardList/>
       </h1>
