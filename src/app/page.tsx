b6e5c1894c6dfc611c2edb8c80acda4865b99dcb
thao <mrtrados@gmail.com>
2023-03-18 13:00:32 +0900

    ADD getList & Role

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { WeatherCardList } from '@/components/WeatherCardList';
 import { SearchBar } from '@/components/SearchBar';
-
+import { getList } from '@/lib/GetList';
+import { Role } from '@/components/Database';
 
 export default function Home() {
   return (
