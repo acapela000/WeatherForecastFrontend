862bf177516fc77f5fbdca0cff65831cd2361fca
thao <mrtrados@gmail.com>
2023-03-22 18:49:07 +0900

    CREATE icon Dictionary

---
+++ src/components/Icon.ts
@@ -0,0 +1,27 @@
+import { faCloudSun,
+    faUmbrella,
+    faSnowflake,
+    faTemperatureHalf,
+    faCloud,
+    faSun,
+    faTornado,
+    faWind, 
+    faGhost,
+    IconDefinition} from "@fortawesome/free-solid-svg-icons";
+
+    
+const iconsDict: { [key: string]: IconDefinition } = {
+    sunny: faSun,
+    cloudy: faCloud,
+    tornado: faTornado,
+    windy: faWind,
+    rainy: faUmbrella,
+    snowy: faSnowflake,
+    hot: faTemperatureHalf,
+    partiallycloudy: faCloudSun
+}
+
+
+export function GetIcon(key: string): IconDefinition {
+    return iconsDict[key] ?? faGhost
+}
