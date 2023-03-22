f3b8bd01203dde6faa00673b75cee9ca8d0c55ad
thao <mrtrados@gmail.com>
2023-03-22 18:53:33 +0900

    UPDATE getById with Generic

--- src/lib/GetById.ts
+++ src/lib/GetById.ts
@@ -1,17 +1,33 @@
-import { Role } from '@/components/Database';
+import { Location, Role, User, WeatherForecast } from '@/components/Database';
 
 
-export function GetRoleById(id: string): Promise<Role> {
-
-    const url: string = `http://localhost:8080/role/${id}`; 
-    const promiseRole: Promise<Role> = fetch(url)
+function GetById<T> (id: string, endpoint: string): Promise<T|null> {
+    const url: string = `http://localhost:8080/${endpoint}/${id}`;
+    return fetch(url)
     .then((response: Response) => {
         if (!response.ok) {
             throw new Error;
         }
-        return response.json() as Promise<Role>;
+        return response.json() as Promise<T>;
     })
+    .catch((error) => {
+        console.log(error)
+        return null;
+    });
+}
 
-    return promiseRole;
+export function GetUserById(id: string): Promise<User|null> {
+    return GetById<User> (id, 'user');
+}
 
+export function GetWFById(id: string): Promise<WeatherForecast|null> {
+    return GetById<WeatherForecast> (id, 'wf');
+}
+
+export function GetRoleById(id: string): Promise<Role|null> {
+        return GetById<Role> (id, 'role');
+}
+
+export function GetLocationById(id: string): Promise<Location|null> {
+        return GetById<Location> (id, 'location');
 }
