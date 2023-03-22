33a92840207ca33624fb9e83fa208c82683475f8
thao <mrtrados@gmail.com>
2023-03-22 18:50:08 +0900

    UPDATE form of SearchBar with css

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -22,13 +22,19 @@ export function SearchBar() {
     return (
         
         <>
-        <form onSubmit={handleSubmit(formData => console.log(formData))}>
-            <input {...register('query', validation)} />
-            <input type="submit"/>
-
-            {errors.query && <p>Please fill the search bar.</p>}
-        </form>
-        
+        <div className="max-w-screen-xl justify-center items-center text-center flex">
+            <form onSubmit={handleSubmit(formData => console.log(formData))} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+                <div className="mb-4">
+                    {/* <label className="block text-gray-700 text-sm font-bold mb-2"> </label> */}
+                    <input {...register('query', validation)} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"/>
+                    {errors.query && <p>Please fill the search bar!</p>}
+                </div>
+                
+                <div>
+                    <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Search</button>
+                </div>
+            </form>
+      </div>
         </>
     )
 }
