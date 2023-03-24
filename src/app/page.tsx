a8e490786f94e4c1efda160fa6ef7bd73eca0051
thao <mrtrados@gmail.com>
2023-03-24 11:03:14 +0900

    ADD function Home() with setLocation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,61 @@
+'use client';
+import { useState, useEffect } from 'react';
+
 import { WeatherCardList } from '@/components/WeatherCardList';
 import { SearchBar } from '@/components/SearchBar';
+import { WeatherCard } from '@/components/WeatherCard';
+import { WeatherForecast } from '@/components/Database';
+import { Location } from '@/components/Database';
+import { GetLocationById } from '@/lib/GetById';
+
 
 export default function Home() {
+  const [location, setLocation] = useState<Location|null>(null);
+  useEffect(() => {
+    //API call
+    const id: string = "2ae0ce5e-c8f5-4792-a44e-9139df0fcc04";
+    GetLocationById(id).then((response) => setLocation(response));
+  }, []);
+
+  const today: WeatherForecast = {
+        icon: 'https://icon-library.com/images/weather-icon-gif/weather-icon-gif-4.jpg',
+        date: new Date(),
+        temperature: 28.5,
+        condition:'hot',
+        isPrecipitating: false,
+        humidity: 0.7
+  }
+
+  if (location == null) {
+    return (
+      <p>Loading...</p>
+    )
+  }
+
   return (
     <>
-    <h1 className='text-2xl bg-lime-100'> 
+    <div>
+      {location.name}
+      {location.state}
+      {location.city}
+      {location.country}
+
+    <h1 className='text-2xl'> 
       <SearchBar/>
     </h1>
     
+    <h1 className='text-2xl'> 
+      <WeatherCard day={today}/>
+    </h1>
     
-    <h1 className='text-2xl bg-cyan-100'>
+    <h1 className='text-2xl'>
       <WeatherCardList/>
-      </h1>
+    </h1>
 
-      <h1>
-        <footer className=' bg-lime-100'></footer>
-      </h1>
+  
+    </div>
+    
+    
     </>
   )
 }
