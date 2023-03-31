4c27c93060d7c02731a415f63fff52f24dc7319f
thao <mrtrados@gmail.com>
2023-03-31 22:16:41 +0900

    ADD geolocation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,13 +10,35 @@ import { GetLocationById } from '@/lib/GetById';
 import { GetLocationByCountryAndCity } from './../lib/Search';
 import { MainCardSkelaton } from '@/components/MainCardSkelaton';
 
+//generate custom geoip model
+interface GeoIP {
+  ip: string;
+  country: string;
+}
 
 export default function Home() {
   const [locationList, setLocationList] = useState<Location[]>([]);
+  const [geoIP, setGeoIP] = useState({ip: '', country: ''});
+
   useEffect(() => {
     //API call
     GetLocationByCountryAndCity()
     .then((response) => setLocationList(response));
+    //geoip api
+    fetch('https://geolocation-db.com/json/')
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error(res.statusText);
+      }
+
+      return res.json() as Promise<{IPv4: string, "country_name": string}>
+    })
+    .then((json) => {
+      const currentGeoIP: GeoIP = {ip: json.IPv4, country: json.country_name}
+      setGeoIP(currentGeoIP)
+      return currentGeoIP;
+    })
+    .catch((err) => console.log(err));
   }, []);
 
   const today: WeatherForecast = {
@@ -48,23 +70,26 @@ const errorCallback = (error: any) => {
   console.log(error);
 };
 
-navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
-const id = navigator.geolocation.watchPosition(successCallback, errorCallback);
-const options = {
-  enableHighAccuracy: true,
-  timeout: 10000,
-};
+//navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+// const id = navigator.geolocation.watchPosition(successCallback, errorCallback);
+// const options = {
+//   enableHighAccuracy: true,
+//   timeout: 10000,
+// };
 
-navigator.geolocation.getCurrentPosition(
-  successCallback,
-  errorCallback,
-  options
-); 
+// navigator.geolocation.getCurrentPosition(
+//   successCallback,
+//   errorCallback,
+//   options
+// ); 
 
 
 return (
     <>
     <div>
+      {geoIP.country}
+      {geoIP.ip}
+
       {locationList.length==0 && <MainCardSkelaton/>}
       {/*simplize step by step*/}
       {/*locationList[0].city.toString()*/}
