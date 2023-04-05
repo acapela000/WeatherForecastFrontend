58d62e3a60c014c055146b7bc2a266bef41e5aa1
thao <mrtrados@gmail.com>
2023-04-05 19:36:31 +0900

    UPDATE bg css

--- src/components/WeatherCard.tsx
+++ src/components/WeatherCard.tsx
@@ -8,25 +8,25 @@ export function WeatherCard(prop: any) {
     const item: WeatherForecast = prop.day;
     const date: string = format(item.date, "LLLL dd");
     const dayOfWeek: string = format(item.date, "EEEE")
-    const themeCSS: any = {
-        bg: item.isPrecipitating ? 'b-gray-400 bg-gray-900' : 'bg-white',
-        fg: item.isPrecipitating ? 'text-amber-500' : 'text-cyan-500',
-        fgLight: item.isPrecipitating ? 'text-amber-500' : 'text-cyan-400'
-    }
+    // const themeCSS: any = {
+    //     bg: item.isPrecipitating ? 'b-gray-400 bg-gray-900' : 'bg-white',
+    //     fg: item.isPrecipitating ? 'text-amber-500' : 'text-cyan-500',
+    //     fgLight: item.isPrecipitating ? 'text-amber-500' : 'text-cyan-400'
+    // }
     
     return (
         <>
-        <div className={themeCSS.bg + ' ' + "w-64 md:mr-20 mb-10 transition duration-500 ease-in-out transform rounded-lg hover:scale-105 cursor-pointer border flex flex-col justify-center items-center text-center p-6"}>
-        <div className={"text-md font-bold flex flex-col" + ' ' + themeCSS.fg}>
+        <div className="bg-transparent w-16 md:mr-20 mb-10 transition duration-500 ease-in-out transform rounded-lg hover:scale-105 cursor-pointer border flex flex-col justify-center items-center text-center p-6">
+        <div className="text-md font-bold flex flex-col bg-transparent">
             <span className="uppercase">{dayOfWeek}</span> 
-            <span className={"font-normal text-sm" +' '+ themeCSS.fgLight}>{date}</span>
+            <span className="font-normal text-sm bg-none">{date}</span>
         </div>
 
         <FontAwesomeIcon icon={GetIcon(item.condition)}
-        className={themeCSS.fg + " " + "hover:shrink-3 w-20 h-20 text-md flex-2 items-center justify-center"}/>
+        className="bg-transparent hover:shrink-3 w-20 h-20 text-md flex-2 items-center justify-center"/>
 
-        <p className={"mb-2" +' '+ themeCSS.fgLight}>{item.condition}-{item.humidity}</p>
-        <div className={themeCSS.fg +' '+ "text-3xl font-bold mb-6"}>{item.temperature}°C</div>
+        <p className="bg-transparent mb-2">{item.condition}-{item.humidity}</p>
+        <div className="bg-transparent text-3xl font-bold mb-6" >{item.temperature}°C</div>
         </div>
         </>
     )
