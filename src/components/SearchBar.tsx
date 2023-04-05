6982a38a194f57c5ce4ef6d1424e87909312600e
thao <mrtrados@gmail.com>
2023-04-05 19:35:52 +0900

    UPDATE bg css

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -22,16 +22,16 @@ export function SearchBar() {
     return (
         
         <>
-        <div className="max-w-screen-xl justify-center items-center text-center flex">
-            <form onSubmit={handleSubmit(formData => console.log(formData))} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <div className="justify-center items-center text-center flex">
+            <form onSubmit={handleSubmit(formData => console.log(formData))} className="bg-none rounded px-8 pt-6 pb-8 mb-4">
                 <div className="mb-4">
                     {/* <label className="block text-gray-700 text-sm font-bold mb-2"> </label> */}
-                    <input {...register('query', validation)} className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"/>
+                    <input {...register('query', validation)} className="shadow appearance-none border rounded w-80 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"/>
+                   <button type="submit" className="bg-blue-100 hover:bg-blue-200 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Search</button>
                     {errors.query && <p>Please fill the search bar!</p>}
                 </div>
                 
                 <div>
-                    <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Search</button>
                 </div>
             </form>
       </div>
