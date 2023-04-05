e475200e574e860d65c07c18147bf1186a3eef37
thao <mrtrados@gmail.com>
2023-04-05 19:36:50 +0900

    ADD mock-data

--- src/components/WeatherCardList.tsx
+++ src/components/WeatherCardList.tsx
@@ -6,26 +6,54 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const itemList: WeatherForecast[] = 
     [
         {
-        icon: 'https://icon-library.com/images/weather-icon-gif/weather-icon-gif-4.jpg',
-        date: new Date(),
-        temperature: 28.5,
-        condition:'hot',
-        isPrecipitating: false,
-        humidity: 0.7},
+            icon: 'https://icon-library.com/images/weather-icon-gif/weather-icon-gif-4.jpg',
+            date: new Date(),
+            temperature: 28.5,
+            condition:'hot',
+            isPrecipitating: false,
+            humidity: 0.7},
         {
-        icon: 'https://icon-library.com/images/weather-icon-gif/weather-icon-gif-3.jpg',
-        date: new Date(),
-        temperature: 20.5,
-        condition:'snowy',
-        isPrecipitating: true,
-        humidity: 0.8},
+            icon: 'https://icon-library.com/images/weather-icon-gif/weather-icon-gif-5.jpg',
+            date: new Date(),
+            temperature: 20.5,
+            condition:'snowy',
+            isPrecipitating: true,
+            humidity: 0.8},
         {
-        icon: 'https://icon-library.com/images/weather-icon-gif/weather-icon-gif-2.jpg',
-        date: new Date(),
-        temperature: 24.5,
-        condition:'tornado',
-        isPrecipitating: false,
-        humidity: 0.9}        
+            icon: 'https://icon-library.com/images/weather-icon-gif/weather-icon-gif-3.jpg',
+            date: new Date(),
+            temperature: 20.5,
+            condition:'windy',
+            isPrecipitating: true,
+            humidity: 0.8},
+        {
+            icon: 'https://icon-library.com/images/weather-icon-gif/weather-icon-gif-3.jpg',
+            date: new Date(),
+            temperature: 20.5,
+            condition:'rainy',
+            isPrecipitating: true,
+            humidity: 0.8},
+        {
+            icon: 'https://icon-library.com/images/weather-icon-gif/weather-icon-gif-4.jpg',
+            date: new Date(),
+            temperature: 20.5,
+            condition:'sunny',
+            isPrecipitating: true,
+            humidity: 0.8},
+        {
+            icon: 'https://icon-library.com/images/weather-icon-gif/weather-icon-gif-1.jpg',
+            date: new Date(),
+            temperature: 20.5,
+            condition:'cloudy',
+            isPrecipitating: true,
+            humidity: 0.8},
+        {
+            icon: 'https://icon-library.com/images/weather-icon-gif/weather-icon-gif-2.jpg',
+            date: new Date(),
+            temperature: 24.5,
+            condition:'tornado',
+            isPrecipitating: false,
+            humidity: 0.9}        
     ]
     
 
@@ -55,6 +83,4 @@ export function WeatherCardList() {
         </div>
         </>
     )
-
-
 }
