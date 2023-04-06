d810e83fe3a45e807d99b3e282a62acfe850ef1f
thao <mrtrados@gmail.com>
2023-04-06 13:13:00 +0900

    UPDATE with WeatherCardList

--- src/app/[country]/[city]/page.tsx
+++ src/app/[country]/[city]/page.tsx
@@ -1,100 +1,33 @@
 'use client';
-import { useState, useEffect } from 'react';
-
-import { LocationCardList } from '@/components/LocationCardList';
-import { SearchBar } from '@/components/SearchBar';
+import { GetCurrentLocation } from '@/lib/location/GetCurrentLocation';
 import { LocationCard } from '@/components/LocationCard';
-import { Location } from '@/components/Database';
-import { GetLocationById } from '@/lib/GetById';
-import { GetLocationByCountryAndCity } from '@/lib/Search';
-import { MainCardSkelaton } from '@/components/MainCardSkelaton';
+import { WeatherCardList } from '@/components/WeatherCardList';
 
-//generate custom geoip model
-interface GeoIP {
-  ip: string;
-  country: string;
-}
 
-export default function Home(props: any) {
-  const [locationList, setLocationList] = useState<Location[]>([]);
-  const [geoIP, setGeoIP] = useState({ip: '', country: ''});
-
-  useEffect(() => {
-    //API call
-    GetLocationByCountryAndCity()
-    .then((response) => setLocationList(response));
-    //geoip api
-    fetch('https://geolocation-db.com/json/')
-    .then((res) => {
-      if (!res.ok) {
-        throw new Error(res.statusText);
-      }
-
-      return res.json() as Promise<{IPv4: string, "country_name": string}>
-    })
-    .then((json) => {
-      const currentGeoIP: GeoIP = {ip: json.IPv4, country: json.country_name}
-      setGeoIP(currentGeoIP)
-      return currentGeoIP;
-    })
-    .catch((err) => console.log(err));
-  }, []);
+export default function Home() {
 
   const thisLocation: Location = {
-        name: 'Luxomi',
+    name: 'AAALuxomi',
         state: 'Dubini',
-        city: props.params.city,
-        country: props.params.country,
+        city: 'Araba',
+        country: 'PARIS',
         weatherForecastList: []
   }
 
-  const locationListHTML = 
-  locationList.map((item) => {
-    return (
-      <div key={item.name}>
-        <p>{item.country}</p>
-        <p>{item.name}</p>
-        <p>{item.city}</p>
-        <p>{item.state}</p>
-      </div>
-    )
-  })
-
-const successCallback = (position: any) => {
-  console.log(position);
-};
-
-const errorCallback = (error: any) => {
-  console.log(error);
-};
-
-//navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
-// const id = navigator.geolocation.watchPosition(successCallback, errorCallback);
-// const options = {
-//   enableHighAccuracy: true,
-//   timeout: 10000,
-// };
-
-// navigator.geolocation.getCurrentPosition(
-//   successCallback,
-//   errorCallback,
-//   options
-// ); 
-
-
 return (
     <>
-    <div>
-      {geoIP.country}
-      {geoIP.ip}
+    <div className='p-5 rounded-lg '>
+      <h1 className='text font-serif'> 
+        <LocationCard locations={thisLocation}/>
+      </h1>
 
-      {locationList.length==0 && <MainCardSkelaton/>}
-      {/*simplize step by step*/}
-      {/*locationList[0].city.toString()*/}
-      {locationListHTML}
-    <h1 className='text-xl'> 
-      <LocationCard locations={thisLocation}/>
-    </h1>
+      {/* <h1 className='text-xl border-none border-collapse'> 
+        <WeatherCard day={today}/>
+      </h1> */}
+      
+      <h1 className='text font-serif'>
+        <WeatherCardList/>
+      </h1>
 
     </div>
     </>
