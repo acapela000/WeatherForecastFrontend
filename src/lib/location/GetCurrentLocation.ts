8cc17de8e1551415b328bb5400a323d589463742
thao <mrtrados@gmail.com>
2023-04-09 21:58:19 +0900

    CREATE GetCurrentLocation in lib

---
+++ src/lib/location/GetCurrentLocation.ts
@@ -0,0 +1,33 @@
+import { GetCountryName } from "@/lib/location/GetCountryName";
+
+
+//generate custom geoip model
+interface GeoIP {
+    country: string;
+    city: string;
+    state: string
+}
+
+
+export async function GetCurrentLocation(): Promise<GeoIP> {
+    return fetch(`https://ipinfo.io/json?token=${process.env.LOCATION_TOKEN}`)
+        .then((res) => {
+            if (!res.ok) {
+                throw new Error(res.statusText);
+            }
+            return res.json() as Promise<{ "country": string, "city": string, "region": string }>
+        })
+        .then((json) => {
+            const currentGeoIP: GeoIP = {
+                country: GetCountryName(json.country),
+                city: json.city,
+                state: json.region
+            }
+            return currentGeoIP;
+            
+        })
+        .catch((err) => {
+            console.log(err)
+            return {country: '', city: '', state: ''};
+        });
+}
