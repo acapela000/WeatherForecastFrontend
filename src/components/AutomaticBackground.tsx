a313aef9ae275ec7f0b5828549c513c356e3373e
thao <mrtrados@gmail.com>
2023-04-09 21:54:38 +0900

    CREATE AutomaticBackground

---
+++ src/components/AutomaticBackground.tsx
@@ -0,0 +1,36 @@
+'use client';
+import { useEffect, useState } from 'react';
+
+
+export default function AutomaticBackground(prop: any) {
+  const [backgroundImg, setBackgroundImage] = useState('');
+
+  useEffect(() => {
+    setBackground();
+    window.addEventListener('resize', setBackground);
+    return () => {
+      window.removeEventListener('resize', setBackground);
+    };
+  }, []);
+
+  const setBackground = () => {
+    const desktopImage = 'https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/6ef35386267195.5d94716770259.png';
+    const mobileImage = 'https://wallpapercave.com/wp/wp7109802.jpg';
+    const screenWidth = window.innerWidth;
+    const screenHeight = window.innerHeight;
+    const ratio = screenWidth / screenHeight;
+
+    if (ratio >= 1.5) {
+      setBackgroundImage(`url(${desktopImage})`);
+    } else {
+      setBackgroundImage(`url(${mobileImage})`);
+    }
+  };
+
+  return (
+    <body className="h-14 bg-cover"
+        style={{ backgroundImage: backgroundImg }}>
+        {prop.children}
+    </body>
+  );
+}
