8dbf18e25b1eb864532c53ca0bf7a3e2103d58ae
thao <mrtrados@gmail.com>
2023-04-09 21:53:18 +0900

    UPDATE name of class: CurrentLocation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,27 @@
 'use client';
 import { SearchBar } from '@/components/SearchBar';
 import { LocationCardList } from '@/components/LocationCardList';
-import { GetCurrentLocation } from '@/lib/location/GetCurrentLocation';
+import { CurrentLocation } from '@/components/CurrentLocation';
 
 export default function Home() {
 
   return (
     <>
-    <div >
+      <div >
 
-    <h1 className='text font-serif'> 
-      <SearchBar/>
-    </h1>
-    
-    <h1 className='text-xl font-serif'>
-      <GetCurrentLocation/>
-    </h1>
-  
-    <h1 className='text-xl font-serif'>
-      <LocationCardList/>
-    </h1>
-    </div>
+        <h1 className='text font-serif'>
+          <SearchBar />
+        </h1>
+
+        <h1 className='text-xl font-serif'>
+          <CurrentLocation />
+        </h1>
+
+        <h1 className='text-xl font-serif'>
+          <LocationCardList />
+        </h1>
+      </div>
     </>
-  )}
+  )
+}
 
