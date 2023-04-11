2b299dd04a77c8dc65e59759224469a9a635c375
thao <mrtrados@gmail.com>
2023-04-11 20:39:59 +0900

    UPDATE rm locationListHTML

--- src/components/CurrentLocation.tsx
+++ src/components/CurrentLocation.tsx
@@ -4,50 +4,41 @@ import { GetLocationByCountryAndCity } from '@/lib/weather-forecast/Search';
 import { useState, useEffect } from 'react';
 import { MainCardSkelaton } from '@/components/MainCardSkelaton';
 import { GetCurrentLocation } from "@/lib/location/GetCurrentLocation";
+import { LocationCard } from '@/components/LocationCard';
 
-  
+
 export function CurrentLocation() {
-    const [locationList, setLocationList] = useState<Location[]>([]);
-    const [geoIP, setGeoIP] = useState({country: '', city: '', state: ''});
-  
-    useEffect(() => {
-      //API call
-      GetLocationByCountryAndCity()
-      .then((response) => setLocationList(response));
-      //geoip api
-      GetCurrentLocation()
-      .then((dataLocation) => setGeoIP(dataLocation))
-    }, []);
+  const [location, setLocation] = useState<Location | null>(null);
 
-    
-    
-      const locationListHTML = 
-      locationList.map((item) => {
-        return (
-          <div key={item.name}>
-            <p>{item.country}</p>
-            <p>{item.name}</p>
-            <p>{item.city}</p>
-            <p>{item.state}</p>
-          </div>
-        )
+  useEffect(() => {
+
+    //geoip api
+    GetCurrentLocation()
+      .then((dataLocation) => {
+        //API call
+        GetLocationByCountryAndCity(dataLocation.country, dataLocation.city)
+          .then((response) => {
+            if (response.length > 0) {
+              setLocation(response[0])
+            }
+          });
       })
-    
-    return (
-        <>
-        <div className='p-5 rounded-lg'>
-          <div className='text-white bg-transparent md:mr-5 transition duration-500 ease-in-out transform hover:scale-105 cursor-pointer border pb-6 pl-6 mb-7 rounded-lg backdrop-blur-sm'>
-            <p> Current Location </p>
-            <span>Country: </span>{geoIP.country}<br/>
-            <span>City: </span>{geoIP.city}<br/>
-            <span>State: </span>{geoIP.state}
-          </div>
-    
-          {/* {locationList.length==0 && <MainCardSkelaton/>} */}
-    
-          {/*simplize step by step*/}
-          {/*locationList[0].city.toString()*/}
-          {/* {locationListHTML} */}
-        </div>
-        </>
-    )}
+  }, []);
+
+
+
+
+  return (
+    <>
+      <div className='p-5 rounded-lg'>
+
+
+        {location != null && <LocationCard location={location}/>}
+
+      {/*simplize step by step*/}
+      {/*locationList[0].city.toString()*/}
+      {/* {locationListHTML} */}
+    </div>
+    </>
+  )
+}
