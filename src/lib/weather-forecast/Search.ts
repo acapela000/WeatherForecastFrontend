78027fdb5bfdc4329b1a1fc85c8ee88de2e5905e
thao <mrtrados@gmail.com>
2023-04-11 20:46:33 +0900

    UPDATE w urlSearchParams,GetLocationCountry&City..

--- src/lib/weather-forecast/Search.ts
+++ src/lib/weather-forecast/Search.ts
@@ -1,19 +1,15 @@
 import { Location } from "@/components/Database";
 import { json } from "stream/consumers";
 
-function getLocationBySearch<T>(endpoint: string, key: string, country: string, city: string): Promise<T[]> {
-    const urlSearchParam: URLSearchParams = new URLSearchParams({
-        country: country.toString(),
-        city: city.toString()
-    });
+function getLocationBySearch<T>(endpoint: string, key: string, urlSearchParam: URLSearchParams): Promise<T[]> {
     const url = `${process.env.API}${endpoint}?${urlSearchParam.toString()}`;
-
+    
     return fetch(url)
     .then((response: Response): Promise<any> => {
-    if (!response.ok) {
-        throw new Error();
-    }
-    return response.json() as Promise<{_embedded: any}>;
+        if (!response.ok) {
+            throw new Error();
+        }
+        return response.json() as Promise<{_embedded: any}>;
     })
     .then((json: any): Promise<any> => {
         return json._embedded as Promise<{ [key: string]: T[] }>;
@@ -27,6 +23,17 @@ function getLocationBySearch<T>(endpoint: string, key: string, country: string, 
     })
 }
 
-export function GetLocationByCountryAndCity(country = 'Japan', city = 'Yokohama') {
-    return getLocationBySearch<Location> ("location/search/country-city", "locationList", country, city);
+export function GetLocationByCountryAndCity(country: string, city: string): Promise<Location[]> {
+    const urlSearchParam: URLSearchParams = new URLSearchParams({
+        country: country.toString(),
+        city: city.toString()
+    });
+    return getLocationBySearch<Location> ("location/search/country-city", "locationList", urlSearchParam);
+}
+
+export function GetLocationByName(name: string): Promise<Location[]> {
+    const urlSearchParam: URLSearchParams = new URLSearchParams({
+        name: name.toString()
+    });
+    return getLocationBySearch<Location> ("location/search/name", "locationList", urlSearchParam);
 }
