c872178fc8f7c8110b0ec0cccb80509b3f6b0c20
thao <mrtrados@gmail.com>
2023-04-11 21:39:32 +0900

    FIX parameter

--- src/app/api/current-place/route.ts
+++ src/app/api/current-place/route.ts
@@ -4,7 +4,7 @@ import { GeoIP, GetCurrentLocation } from "@/lib/location/GetCurrentLocation";
 
 export async function GET() {
   const token: string = process.env.LOCATION_TOKEN ?? '';
-  const currentPlace: GeoIP = await GetCurrentLocation(token);
+  const currentPlace: GeoIP = await GetCurrentLocation();
 
   return NextResponse.json(currentPlace);
 }
