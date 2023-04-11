cbbf55bdb759b7639217d6cfd766cf0f10ab5f37
thao <mrtrados@gmail.com>
2023-04-11 20:44:04 +0900

    UPDATE rm mockDT,itemList with wfList,+props

--- src/components/WeatherCardList.tsx
+++ src/components/WeatherCardList.tsx
@@ -3,61 +3,62 @@ import { WeatherCard } from "./WeatherCard";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 
-const itemList: WeatherForecast[] = 
-    [
-        {
-            icon: 'https://icon-library.com/images/weather-icon-gif/weather-icon-gif-4.jpg',
-            date: new Date(),
-            temperature: 28.5,
-            condition:'hot',
-            isPrecipitating: false,
-            humidity: 0.7},
-        {
-            icon: 'https://icon-library.com/images/weather-icon-gif/weather-icon-gif-5.jpg',
-            date: new Date(),
-            temperature: 20.5,
-            condition:'snowy',
-            isPrecipitating: true,
-            humidity: 0.8},
-        {
-            icon: 'https://icon-library.com/images/weather-icon-gif/weather-icon-gif-3.jpg',
-            date: new Date(),
-            temperature: 20.5,
-            condition:'windy',
-            isPrecipitating: true,
-            humidity: 0.8},
-        {
-            icon: 'https://icon-library.com/images/weather-icon-gif/weather-icon-gif-3.jpg',
-            date: new Date(),
-            temperature: 20.5,
-            condition:'rainy',
-            isPrecipitating: true,
-            humidity: 0.8},
-        {
-            icon: 'https://icon-library.com/images/weather-icon-gif/weather-icon-gif-4.jpg',
-            date: new Date(),
-            temperature: 20.5,
-            condition:'sunny',
-            isPrecipitating: true,
-            humidity: 0.8},
-        {
-            icon: 'https://icon-library.com/images/weather-icon-gif/weather-icon-gif-1.jpg',
-            date: new Date(),
-            temperature: 20.5,
-            condition:'cloudy',
-            isPrecipitating: true,
-            humidity: 0.8},
-        {
-            icon: 'https://icon-library.com/images/weather-icon-gif/weather-icon-gif-2.jpg',
-            date: new Date(),
-            temperature: 24.5,
-            condition:'tornado',
-            isPrecipitating: false,
-            humidity: 0.9}        
-    ]
+// const itemList: WeatherForecast[] = 
+//     [
+//         {
+//             icon: '',
+//             date: new Date(),
+//             temperature: 28.5,
+//             condition:'hot',
+//             isPrecipitating: false,
+//             humidity: 0.7},
+//         {
+//             icon: 'https://icon-library.com/images/weather-icon-gif/weather-icon-gif-5.jpg',
+//             date: new Date(),
+//             temperature: 20.5,
+//             condition:'snowy',
+//             isPrecipitating: true,
+//             humidity: 0.8},
+//         {
+//             icon: 'https://icon-library.com/images/weather-icon-gif/weather-icon-gif-3.jpg',
+//             date: new Date(),
+//             temperature: 20.5,
+//             condition:'windy',
+//             isPrecipitating: true,
+//             humidity: 0.8},
+//         {
+//             icon: 'https://icon-library.com/images/weather-icon-gif/weather-icon-gif-3.jpg',
+//             date: new Date(),
+//             temperature: 20.5,
+//             condition:'rainy',
+//             isPrecipitating: true,
+//             humidity: 0.8},
+//         {
+//             icon: 'https://icon-library.com/images/weather-icon-gif/weather-icon-gif-4.jpg',
+//             date: new Date(),
+//             temperature: 20.5,
+//             condition:'sunny',
+//             isPrecipitating: true,
+//             humidity: 0.8},
+//         {
+//             icon: 'https://icon-library.com/images/weather-icon-gif/weather-icon-gif-1.jpg',
+//             date: new Date(),
+//             temperature: 20.5,
+//             condition:'cloudy',
+//             isPrecipitating: true,
+//             humidity: 0.8},
+//         {
+//             icon: 'https://icon-library.com/images/weather-icon-gif/weather-icon-gif-2.jpg',
+//             date: new Date(),
+//             temperature: 24.5,
+//             condition:'tornado',
+//             isPrecipitating: false,
+//             humidity: 0.9}        
+//     ]
     
 
-export function WeatherCardList() {
+export function WeatherCardList(props: any) {
+    const wfList: WeatherForecast[] = props.wf ?? [];
     // const itemHTMLList = itemList.map((item, index) => {
     
     // return (
@@ -68,9 +69,10 @@ export function WeatherCardList() {
     //     </>
     // )
     // }) 
+    // console.log(itemList)
 
     const itemHTMLList = [];
-    for(var item of itemList) {
+    for(var item of wfList) {
         itemHTMLList.push(
         <WeatherCard key={item.icon} day={item}/>
         )
@@ -78,7 +80,7 @@ export function WeatherCardList() {
     
     return(
         <>
-        <div className="m-10 items-center flex flex-col md:flex-row md:justify-center">
+        <div className="m-8 grid grid-cols-3 xs:grid-cols-2 md:grid-cols-7 gap-5">
         {itemHTMLList}
         </div>
         </>
