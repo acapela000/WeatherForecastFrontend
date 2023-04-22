eb08f15b44bd92a6baae414bb5155d4ec79d667b
thao <mrtrados@gmail.com>
2023-04-23 00:24:19 +0900

    UPDATE place.city & state to locationCard

--- src/components/LocationCard.tsx
+++ src/components/LocationCard.tsx
@@ -11,8 +11,10 @@ export function LocationCard(prop: any) {
         <>
         <Link href={place.country +"/"+ place.city}>
         
-        <div className="mt-10 text-white bg-transparent md:mr-5 transition duration-500 ease-in-out transform rounded-lg hover:scale-105 cursor-pointer border pb-6 pl-6 mb-6 rounded-lg backdrop-blur-sm">
-            <div>{place.country} </div>
+        <div className="mt-10 text-white bg-transparent md:mr-5 transition duration-500 ease-in-out transform hover:scale-105 cursor-pointer border pb-6 pl-6 mb-6 rounded-lg backdrop-blur-sm">
+            <div>{place.country}</div>
+            <div>{place.city}</div>
+            <div>{place.state}</div>
             <div className="">
                 {place.weatherForecastList?.find((els: WeatherForecast) => {
                     return els.lastUpdated.toISOString().split('T')[0] == today.toISOString().split('T')[0]
@@ -24,12 +26,4 @@ export function LocationCard(prop: any) {
     )}
 
     // https://stackoverflow.com/questions/2013255/how-to-get-year-month-day-from-a-date-object
-    // bg-red-100
-    // bg-rose-100
-    // bg-orange-100
-    // bg-amber-100
-    // bg-lime-100
-    // bg-green-100
-    // bg-cyan-100
-    // bg-purple-100
-    // bg-pink-100
+  
