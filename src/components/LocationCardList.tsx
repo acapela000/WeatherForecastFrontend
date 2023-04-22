cd570acee932e2e34bec25e12af2bc94c1c9cfeb
thao <mrtrados@gmail.com>
2023-04-23 00:24:50 +0900

    UPDATE rm mock-data

--- src/components/LocationCardList.tsx
+++ src/components/LocationCardList.tsx
@@ -3,46 +3,7 @@ import { LocationCard } from "./LocationCard";
 import { WeatherCard } from "./WeatherCard";
 import { WeatherCardList } from "./WeatherCardList";
 
-const placeList: Location[] = 
-    [
-        // {name: 'Luxomi',
-        // state: 'Dubini',
-        // city: 'Morocco',
-        // country: 'Norway',
-        // weatherForecastList: []},
-        // {name: 'Luxomi',
-        // state: 'Dubini',
-        // city: 'Yelmin',
-        // country: 'Geogia',
-        // weatherForecastList: [
-        //     {icon: '',
-        //     date: new Date(),
-        //     temperature: 30.5,
-        //     condition:'hot',
-        //     isPrecipitating: false,
-        //     humidity: 0.7}
-        // ]},
-        // {name: 'Luxomi',
-        // state: 'Dubini',
-        // city: 'Araba',
-        // country: 'Findland',
-        // weatherForecastList: []},
-        // {name: 'Melbourn',
-        // state: 'Perth',
-        // city: 'Sydney',
-        // country: 'Australia',
-        // weatherForecastList: []},
-        // {name: 'Honolulu',
-        // state: 'Dubini',
-        // city: 'Hannan',
-        // country: 'Hungary',
-        // weatherForecastList: []},
-        // {name: 'Macao',
-        // state: 'Dubini',
-        // city: 'Rufelo',
-        // country: 'Ireland',
-        // weatherForecastList: []}       
-    ]
+const placeList: Location[] = [];
     
 
 export function LocationCardList(props: any) {
